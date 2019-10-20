0ef035c4ea749e36ac88b2a74b990fee87051f20
Norman Chen <normchenjk@users.noreply.github.com>
2019-10-20 15:19:11 +1100

    Integrating Google Cloud Functions into an Angular application

--- image-gallery/src/app/app.component.ts
+++ image-gallery/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'image-gallery';
+
+  selectedFile: File;
+  newWidth: number;
+  newHeight: number;
+
+  thumbnail: string | ArrayBuffer;
+
+  constructor(private http: HttpClient) { }
+
+  onSelectFile(files: FileList) {
+    this.selectedFile = files.item(0);
+  }
+
+  onSubmitForm() {
+    const formData = new FormData();
+    formData.append('file', this.selectedFile, this.selectedFile.name);
+    formData.append('newWidth', this.newWidth.toString());
+    formData.append('newHeight', this.newHeight.toString());
+
+    const url = "https://us-central1-fit2095-2019s2-1.cloudfunctions.net/createThumbnail";
+    this.http.post<any>(url, formData, { responseType: 'blob' as 'json' }).subscribe(
+      (res) => {
+        // Convert image which is in binary form, into a format understandable by the <img> HTML element
+        const reader = new FileReader();
+        reader.readAsDataURL(res);
+        reader.onloadend = () => {
+          this.thumbnail = reader.result;
+        }
+      },
+      (err) => {
+        this.thumbnail = null;
+      }
+    );
+  }
 }
--- image-gallery/src/app/app.module.ts
+++ image-gallery/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
